3a25783861b66728108a6f48d51d868ffd6aae17
WuChenDi <wuchendi96@gmail.com>
2023-01-31 17:47:34 +0800

    feat: update backup script file

--- script/check-keyword.old.sh
+++ script/check-keyword.old.sh
@@ -101,16 +101,55 @@
 # done
 
 # Case 6
+# # 设置需要过滤的关键字(支持中文)
+# FILTER_WORDS="公司 debugger"
+
+# # 设置不需要检测的目录或文件路径
+# IGNORE_PATHS=".git node_modules dist script"
+
+# # 获取暂存区中的文件列表
+# FILES=$(git diff --name-only --cached)
+
+# # 循环遍历文件列表
+# for FILE in $FILES; do
+#   # 判断文件是否在不需要检测的目录或文件路径中
+#   IGNORE=false
+#   for IGNORE_PATH in $IGNORE_PATHS; do
+#     if [[ $FILE == *"$IGNORE_PATH"* ]]; then
+#       IGNORE=true
+#       break
+#     fi
+#   done
+#   if $IGNORE; then
+#     continue
+#   fi
+
+#   # 判断文件中是否存在需要过滤的关键字
+#   for FILTER_WORD in $FILTER_WORDS; do
+#     if grep -Eiq "$FILTER_WORD" "$FILE"; then
+#       echo -e "\033[31m[警告]\033[0m 文件 $FILE 中存在关键字 $FILTER_WORD"
+#       exit 1
+#     fi
+#   done
+# done
+
+# echo -e "\033[32m[提示]\033[0m 没有发现需要过滤的关键字"
+# exit 0
+
+# Case 7
 # 设置需要过滤的关键字(支持中文)
-FILTER_WORDS="公司 debugger"
+FILTER_WORDS="debugger 测试"
 
 # 设置不需要检测的目录或文件路径
-IGNORE_PATHS=".git node_modules dist script"
+IGNORE_PATHS=".git node_modules script src/App.vue README.md README-zh_CN.md"
 
-# 获取暂存区中的文件列表
+# 获取所有已暂存的文件的文件名列表
 FILES=$(git diff --name-only --cached)
 
-# 循环遍历文件列表
+# 定义是否发现错误标志
+has_error=false
+
+# 遍历所有文件
 for FILE in $FILES; do
   # 判断文件是否在不需要检测的目录或文件路径中
   IGNORE=false
@@ -127,11 +166,16 @@ for FILE in $FILES; do
   # 判断文件中是否存在需要过滤的关键字
   for FILTER_WORD in $FILTER_WORDS; do
     if grep -Eiq "$FILTER_WORD" "$FILE"; then
-      echo -e "\033[31m[警告]\033[0m 文件 $FILE 中存在关键字 $FILTER_WORD"
-      exit 1
+      echo -e "\033[31m[警告]\033[0m 文件 $FILE 中存在关键字: \033[31m$FILTER_WORD\033[0m"
+      has_error=true
     fi
   done
 done
+
+# 如果发现错误，退出
+if $has_error; then
+  exit 1
+fi
 
 echo -e "\033[32m[提示]\033[0m 没有发现需要过滤的关键字"
 exit 0
